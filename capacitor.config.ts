308bfa484a25bccf4de65d1b75299486308e2ad3
taheito26-sys <taheito26@gmail.com>
2026-03-31 20:48:02 +0300

    fix

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: "app.lovable.b87c5fb9e33348908903e81584334e4b",
+  appId: "com.taheito26sys.corerefactorinitiative",
   appName: "happy-trader-hub",
   webDir: "dist",
   server: {
